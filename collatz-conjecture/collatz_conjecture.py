789bf8f0b27af701260b8f2d3452daf4453b023a
Francisco Prata <fcopratan@gmail.com>
2024-04-02 08:04:26 -0300

    Refactor collatz_conjecture.py to use a while loop for improved readability and efficiency

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,19 +1,20 @@
-count = 0
-
-
 def steps(number):
-    global count
+    """
+    Calculates the number of steps required to reach 1 using the Collatz Conjecture.
+
+    Args:
+        number (int): The starting number.
+
+    Returns:
+        int: The number of steps required to reach 1.
+
+    Raises:
+        ValueError: If the input number is less than 1.
+    """
     if number < 1:
-        raise ValueError("Only positive integers are allowed")
-    if number % 2 == 0:
-        count += 1
-        print(number)
-        return steps(number // 2)
-    if number == 1:
-        count += 1
-        print(number)
-        return count
-    else:
-        count += 1
-        print(number)
-        return steps(3 * number + 1)
+        raise ValueError('Only positive integers are allowed')
+    counter = 0
+    while number > 1:
+        number = 3 * number + 1 if number % 2 else number // 2
+        counter += 1
+    return counter
