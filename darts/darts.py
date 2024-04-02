5d3a820bd0744be5c1f58c6e94bf42af3bcfe899
Francisco Prata <fcopratan@gmail.com>
2024-04-02 15:20:35 -0300

    Refactor score function in darts.py

--- darts/darts.py
+++ darts/darts.py
@@ -16,6 +16,6 @@ def score(x, y):
     distance = (x ** 2 + y ** 2) ** 0.5
 
     if distance <= 1: return 10
-    elif distance <= 5: return 5
-    elif distance <= 10: return 1
+    if distance <= 5: return 5
+    if distance <= 10: return 1
     return 0
