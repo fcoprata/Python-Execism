a220d5b221da13d78a81594e17f70905fd9139e2
Francisco Prata <fcopratan@gmail.com>
2024-04-02 09:09:48 -0300

    Add Darts exercise files and metadata

---
+++ darts/darts.py
@@ -0,0 +1,21 @@
+def score(x, y):
+    """
+    Calculates the score based on the coordinates (x, y) on a dartboard.
+
+    Args:
+        x (float): The x-coordinate of the dart's landing point.
+        y (float): The y-coordinate of the dart's landing point.
+
+    Returns:
+        int: The score based on the distance from the center of the dartboard.
+            - If the distance is less than or equal to 1, the score is 10.
+            - If the distance is less than or equal to 5, the score is 5.
+            - If the distance is less than or equal to 10, the score is 1.
+            - Otherwise, the score is 0.
+    """
+    distance = (x ** 2 + y ** 2) ** 0.5
+
+    if distance <= 1: return 10
+    elif distance <= 5: return 5
+    elif distance <= 10: return 1
+    return 0
