4668028ab8aef8939dbf2eb3ea59f39510822763
Francisco Prata <fcopratan@gmail.com>
2024-04-04 08:31:23 -0300

    Refactor ISBN verification logic

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -11,9 +11,7 @@ def is_valid(isbn):
     isbn = list(isbn.replace('-', ''))
     if len(isbn) != 10: return False
     if isbn[-1] == 'X': isbn[-1] = "10"
-    r = 0
-    for i in range(10):
-        try:
-            r += int(isbn[i]) * (10-i)
-        except ValueError: return False
-    return r % 11 == 0
+    try:
+        total = sum(i * n for i, n in enumerate(isbn))
+    except ValueError: return False
+    return total % 11 == 0
