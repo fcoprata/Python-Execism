11cf468d1bd1d2b6d40e18ab4d26e37181038860
Francisco Prata <fcopratan@gmail.com>
2024-04-04 08:56:18 -0300

    Add implementation for color_code() and colors() functions

--- resistor-color/resistor_color.py
+++ resistor-color/resistor_color.py
@@ -1,6 +1,7 @@
 def color_code(color):
-    pass
+    colors_list = colors()
+    return colors_list.index(color)
 
 
 def colors():
-    pass
+    return ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
