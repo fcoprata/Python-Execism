42620572af5fc10360659aa0f26baf5e210f7653
Francisco Prata <fcopratan@gmail.com>
2024-04-09 15:49:43 -0300

    Implement rotational cipher encryption in rotational_cipher.py

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,21 @@
+chars = "abcdefghijklmnopqrstuvwxyz"
+
+
 def rotate(text, key):
-    pass
+    """
+    Applies a rotational cipher to the given text using the specified key.
+
+    Args:
+        text (str): The text to be encrypted.
+        key (int): The rotation key.
+
+    Returns:
+        str: The encrypted text.
+
+    Example:
+        >>> rotate("Hello, World!", 13)
+        'Uryyb, Jbeyq!'
+    """
+    newchars = chars[key:] + chars[:key]
+    trans = str.maketrans(chars + chars.upper(), newchars + newchars.upper())
+    return text.translate(trans)
