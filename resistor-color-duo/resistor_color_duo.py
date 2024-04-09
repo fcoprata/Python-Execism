5ebf866b5b4d3f95411608f617a014f3760b5e75
Francisco Prata <fcopratan@gmail.com>
2024-04-09 15:43:12 -0300

    Refactor resistor-color-duo implementation to use a dictionary for color codes

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,19 @@
-def value(colors):
-    pass
+codes = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4,
+         'green': 5, 'blue': 6, 'violet': 7, 'grey': 8, 'white': 9}
+
+
+def value(colors: list) -> int:
+    """
+    Calculates the value of a resistor based on the color bands.
+
+    Args:
+        colors (list): A list of color codes representing the color bands of the resistor.
+
+    Returns:
+        int: The numerical value of the resistor.
+
+    """
+    color_one = colors[0]
+    color_two = colors[1]
+
+    return int(str(codes[color_one]) + str(codes[color_two]))
