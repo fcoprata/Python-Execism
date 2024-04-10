0dd94687ca17cee19bf513b34460d34077697d64
Francisco Prata <fcopratan@gmail.com>
2024-04-10 09:20:18 -0300

    Add square root exercise files and metadata

---
+++ square-root/square_root.py
@@ -0,0 +1,11 @@
+def square_root(number):
+    """
+    Calculates the square root of a given number.
+
+    Args:
+        number (int or float): The number for which to calculate the square root.
+
+    Returns:
+        float: The square root of the given number.
+    """
+    return number ** 0.5
