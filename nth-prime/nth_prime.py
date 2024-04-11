dd060ff236b01bc35300dea653b477b979c5672f
Francisco Prata <fcopratan@gmail.com>
2024-04-11 14:14:26 -0300

    Refactor prime function to improve efficiency and readability in nth_prime.py

--- nth-prime/nth_prime.py
+++ nth-prime/nth_prime.py
@@ -1,12 +1,41 @@
-def prime(number):
-    number = abs(number)
-    if number < 2:
-        raise ValueError('there is no zeroth prime')
-    n = 2
-    primes = [2]
-    while len(primes) < number:
-        n += 1
-        if all(n % p > 0 for p in primes):
-            primes.append(n)
+def isPrime(number):
+    """
+    Check if a number is prime.
 
-    return primes[number - 1]
+    Args:
+        number (int): The number to check.
+
+    Returns:
+        bool: True if the number is prime, False otherwise.
+    """
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def prime(number):
+    """
+    Returns the nth prime number.
+
+    Args:
+        number (int): The position of the prime number to be returned.
+
+    Returns:
+        int: The nth prime number.
+
+    Raises:
+        ValueError: If the input number is 0.
+
+    """
+    if number == 0:
+        raise ValueError('there is no zeroth prime')
+    i = 2
+    count = 0
+    while True:
+        if isPrime(i):
+            count += 1
+        if count == number:
+            break
+        i += 1
+    return i
